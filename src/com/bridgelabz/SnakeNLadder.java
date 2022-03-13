175c9ab9f573e0046fb066d5624ab508fd7052c3
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-03-14 02:13:39 +0530

    Added:UC1

---
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+public class SnakeNLadder {
+	
+
+	public static void main(String[] args) {
+		SnakeNLadder1 s = new SnakeNLadder1();
+		s.startGame();
+	}
+
+}
+class SnakeNLadder1
+{
+	public void startGame()
+	{
+		int player1 =0;
+		int currentPlayer=-1;
+	}
+}
