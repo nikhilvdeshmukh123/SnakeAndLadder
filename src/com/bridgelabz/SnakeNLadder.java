9e9a856a7e62a71e0cfaebc8538a563cb38d5794
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-03-15 20:13:53 +0530

    Added:UC-1

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -1,19 +1,18 @@
 package com.bridgelabz;
-
 public class SnakeNLadder {
 	
 
 	public static void main(String[] args) {
-		
-		System.out.println("welcome to snake and ladder game");
-
 		SnakeNLadder1 s = new SnakeNLadder1();
-			s.startGame();
+		s.startGame();
 	}
+
 }
-class SnakeNLadder1 {
-	public void startGame() {
-		int player1 = 0;
-		int currentPlayer = -1;
+class SnakeNLadder1
+{
+	public void startGame()
+	{
+		int player1 =0;
+		int currentPlayer=-1;
 	}
 }
